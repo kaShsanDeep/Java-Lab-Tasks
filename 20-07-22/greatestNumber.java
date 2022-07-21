e898d52ca7acb168e5734f4d7a57442737b8f344
sandeep kumar <66101879+kaShsanDeep@users.noreply.github.com>
2022-07-21 17:16:11 +0530

    greatest of two numbers

---
+++ 20-07-22/greatestNumber.java
@@ -0,0 +1,15 @@
+class greatestNumber
+{
+  public static void main(String args[])
+
+ {
+    int num1=55,num2=87;
+    if (num1>num2)
+    {
+    System.out.println("the greatest number is" +num1 );
+    }  
+    else
+    System.out.println("the greatest number is" +num2); 
+ }
+
+}
