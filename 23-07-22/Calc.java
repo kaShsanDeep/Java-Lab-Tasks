b3fa69d5f8ad0342bc81bfa8a21134172297f06b
sandeep kumar <66101879+kaShsanDeep@users.noreply.github.com>
2022-07-23 17:23:57 +0530

    Add files via upload

---
+++ 23-07-22/Calc.java
@@ -0,0 +1,63 @@
+//making a calculator for
+
+
+import java.util.Scanner;
+class Calc
+{
+  public static void main (String args[] )
+  {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("=======enter the 1st number=======");
+   double firstnumber = sc.nextDouble();
+   System.out.println("=======choose the operator=======\n ======== +,-,/,%,========");
+   char operator;
+   operator = sc.next().charAt(0);
+   
+   System.out.println("=======enter the 2nd number=======");
+   double secondnumber = sc.nextDouble();
+ 
+    switch(operator)
+      {
+     case '+' :
+          {   
+         System.out.println("=====your answer is======\n" +(firstnumber+secondnumber));
+         break;     
+           }
+       case '-' :
+            {   
+         System.out.println("=====your answer is====\n" +(firstnumber-secondnumber));
+         break;
+             }
+
+         case '/' :
+            {   
+         System.out.println("=====your answer is=====\n" +(firstnumber/secondnumber));
+         break;
+             }
+         case '*' :
+            {   
+         System.out.println("=====your answer is====\n" +(firstnumber*secondnumber));
+         break;
+             }
+         case '%' :
+             {
+         System.out.println("=====your answer is===\n" +(firstnumber%secondnumber));   
+         break;
+              }
+
+         default :
+            {   
+         System.out.println("selct proparly");
+         return;
+             }
+
+
+       }  
+
+
+
+   }
+
+
+
+}
