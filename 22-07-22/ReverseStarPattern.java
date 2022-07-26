dafbf99d03ea969320123849cc4aab09090b2f6e
sandeep kumar <66101879+kaShsanDeep@users.noreply.github.com>
2022-07-26 21:43:15 +0530

    reversing the star pattern

---
+++ 22-07-22/ReverseStarPattern.java
@@ -0,0 +1,23 @@
+// writing a code to perform a pattern of "*" for n times using java
+
+import java.util.Scanner;             //loading scanner class for  user input 
+class ReverseStarPattern
+{
+     public static void main(String args[])        
+     {
+	Scanner sc= new Scanner(System.in);
+	System.out.println("Enter  the no");     //print statement to knoow the value
+	int num = sc.nextInt();                  // to have integer type of inputs 
+	
+	for(int i= num;i>=1;i--)                  //using nested for loop  here and i will count till "num" if i is less then num then nested for loop will start
+	 {
+	  for(int j=1;j<=i;j++)                 
+	    {
+           System.out.print('*');               
+	    }
+	    System.out.println("");              // to print output to next line
+	  }
+
+     }
+
+}
